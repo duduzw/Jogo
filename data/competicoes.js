f5110f14dfcf870fc50ff52cff95ab95a5546b25
Dudu <duducraft52@gmail.com>
2026-06-21 20:42:13 -0300

    Add files via upload

---
+++ data/competicoes.js
@@ -0,0 +1,144 @@
+export const competicoes = [
+
+    // BRASIL
+    { id: "br_1", nome: "Brasileirão Série A", tipo: "liga", div: 1, logo: "https://tmssl.akamaized.net//images/logo/header/bra1.png?lm=1713364599" },
+    { id: "br_2", nome: "Brasileirão Série B", tipo: "liga", div: 2, logo: "https://tmssl.akamaized.net//images/logo/header/bra2.png?lm=1731436730" },
+    { id: "br_3", nome: "Brasileirão Série C", tipo: "liga", div: 3, logo: "https://assets.football-logos.cc/logos/brazil/512x512/brazilian-serie-c.76235b53.png" },
+    { id: "br_4", nome: "Brasileirão Série D", tipo: "liga", div: 4, logo: "https://tmssl.akamaized.net//images/logo/header/bra4.png?lm=1682608780" },
+    { id: "copa_br", nome: "Copa do Brasil", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/brazil/512x512/copa-do-brasil--no-text.98d6c38e.png" },
+    { id: "supercopa_br", nome: "Supercopa do Brasil", tipo: "supercopa", div: 1, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Supercopa_Rei_logo_%282023%29.svg/250px-Supercopa_Rei_logo_%282023%29.svg.png" },
+
+    // ARGENTINA
+    { id: "arg_1", nome: "Liga Profesional Argentina", tipo: "liga", div: 1, logo: "https://www.ole.com.ar/collections/escudos/league/581t4mywybx21wcpmpykhyzr3.png" },
+    { id: "copa_arg", nome: "Copa Argentina", tipo: "copa", div: 1, logo: "https://upload.wikimedia.org/wikipedia/pt/thumb/a/aa/Copa_argentina_logo22.png/250px-Copa_argentina_logo22.png" },
+    { id: "supercopa_arg", nome: "Supercopa Argentina", tipo: "supercopa", div: 1, logo: "https://imgs.search.brave.com/HUqgLL_UTKn5_ba-oxKU5POvvAeVfISBW-B5Y8SUnMY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aW1nLnplcm96ZXJv/LnB0L2ltZy9sb2dv/cy9jb21wZXRpY29l/cy8yMTEyX2ltZ2Jh/bmtfc2NfMjAyNTAy/MjYxNjM1NTYucG5n" },
+    
+    //URUGUAI
+    { id: "uy_1", nome: "Primera División Uruguaya", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/uruguay/512x512/liga-auf-uruguaya.619c9202.png" },
+
+    // PORTUGAL
+    { id: "pt_1", nome: "Liga Portugal", tipo: "liga", div: 1, logo: "https://tmssl.akamaized.net//images/logo/homepageWappen150x150/po1.png?lm=1688630708" },
+    { id: "pt_2", nome: "Liga Portugal 2", tipo: "liga", div: 2, logo: "https://assets.football-logos.cc/logos/portugal/512x512/liga-portugal-2.90a1494e.png" },
+    { id: "copa_pt", nome: "Taca de Portugal", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/portugal/512x512/taca-de-portugal.7a663fe9.png" },
+    { id: "supercopa_pt", nome: "Supertaca de Portugal", tipo: "supercopa", div: 1, logo: "https://cdn-img.staticzz.com/img/logos/competicoes/49_imgbank_st_20150709162635.png" },
+    
+    // INGLATERRA
+    { id: "eng_1", nome: "Premier League", tipo: "liga", div: 1, logo: "https://tmssl.akamaized.net//images/logo/header/gb1.png?lm=1521104656" },
+    { id: "eng_2", nome: "Championship", tipo: "liga", div: 2, logo: "https://assets.football-logos.cc/logos/england/512x512/efl-championship.2766b536.png" },
+    { id: "copa_eng", nome: "FA Cup", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/england/512x512/emirates-fa-cup.cb30a62f.png" },
+    { id: "carabao_eng", nome: "Carabao Cup", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/england/512x512/efl-cup.667da864.png" },
+    { id: "supercopa_eng", nome: "Community Shield", tipo: "supercopa", div: 1, logo: "https://assets.football-logos.cc/logos/england/512x512/fa-community-shield.3436f44c.png" },
+
+    // ESPANHA
+    { id: "esp_1", nome: "La Liga", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/spain/512x512/la-liga.0ea0b0c5.png" },
+    { id: "esp_2", nome: "La Liga 2", tipo: "liga", div: 2, logo: "https://assets.football-logos.cc/logos/spain/512x512/la-liga-2.c56096b7.png" },
+    { id: "copa_esp", nome: "Copa del Rey", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/spain/512x512/copa-del-rey.d48736b4.png" },
+    { id: "supercopa_esp", nome: "Supercopa da Espanha", tipo: "supercopa", div: 1, logo: "https://assets.football-logos.cc/logos/spain/512x512/supercopa-de-espana.1b9709b2.png" },
+
+    // ITÁLIA
+    { id: "ita_1", nome: "Serie A", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/italy/512x512/serie-a.1289b16a.png" },
+    { id: "ita_2", nome: "Serie B", tipo: "liga", div: 2, logo: "https://tmssl.akamaized.net//images/logo/header/it2.png?lm=1720690779" },
+    { id: "copa_ita", nome: "Coppa Italia", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/italy/512x512/coppa-italia.c154e4b2.png" },
+    { id: "supercopa_ita", nome: "Supercoppa Italiana", tipo: "supercopa", div: 1, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Logo_EA_SPORTS_FC_SuperCup_2024-2025.jpg/250px-Logo_EA_SPORTS_FC_SuperCup_2024-2025.jpg" },
+    
+    // ALEMANHA
+    { id: "ger_1", nome: "Bundesliga", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/germany/512x512/bundesliga.24d9c6f9.png" },
+    { id: "ger_2", nome: "Bundesliga 2", tipo: "liga", div: 2, logo: "https://assets.football-logos.cc/logos/germany/512x512/2-bundesliga.11755b17.png" },
+    { id: "copa_ger", nome: "DFB-Pokal", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/germany/512x512/dfb-pokal.70f47e30.png" },
+    { id: "supercopa_ger", nome: "DFL-Supercup", tipo: "supercopa", div: 1, logo: "https://upload.wikimedia.org/wikipedia/en/thumb/1/15/Franz_Beckenbauer_Supercup_logo.svg/250px-Franz_Beckenbauer_Supercup_logo.svg.png" },
+    
+    // FRANÇA
+    { id: "fra_1", nome: "Ligue 1", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/france/512x512/ligue-1.bcb13eb0.png" },
+    { id: "fra_2", nome: "Ligue 2", tipo: "liga", div: 2, logo: "https://assets.football-logos.cc/logos/france/512x512/ligue-2.f12004c1.png" },
+    { id: "copa_fra", nome: "Coupe de France", tipo: "copa", div: 1, logo: "https://assets.football-logos.cc/logos/france/512x512/french-cup.10cbf91f.png" },
+    { id: "supercopa_fra", nome: "Trophee des Champions", tipo: "supercopa", div: 1, logo: "https://cdnbfc.com/dq4uxsxpv/france-trophee-des-champions-super-cup.webp" },
+    
+     // HOLANDA
+    { id: "nl_1", nome: "Eredivise", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/netherlands/512x512/eredivisie--no-text.144d4f29.png" },
+    { id: "copa_nl", nome: "KNVB Cup", tipo: "copa", div: 1, logo: "https://imgs.search.brave.com/hxX6E8dsU_hgiLt7P7kNZSeFpSHx8a7ZY7QEPQpMYno/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/bGlibG9nby5jb20v/aW1nLWxvZ28va244/NzNkMjJhLWtudmIt/bG9nby1kdXRjaC1r/bnZiLWJla2VyLXRh/YmxlLWVzcG4ucG5n" },
+    { id: "supercopa_nl", nome: "Johan Cruyff Shield", tipo: "supercopa", div: 1, logo: "https://static.flashscore.com/res/image/data/G8PblNiD-xYIcahVG.png" },
+    
+    // TURQUIA
+    { id: "tr_1", nome: "Süper Lig", tipo: "liga", div: 1, logo: "https://upload.wikimedia.org/wikipedia/pt/thumb/6/6f/Trendyol_S%C3%BCper_Lig_2024_logo.png/250px-Trendyol_S%C3%BCper_Lig_2024_logo.png" },
+    { id: "copa_tr", nome: "Turkish Cup", tipo: "copa", div: 1, logo: "https://static.flashscore.com/res/image/data/QexI2ehD-dOluB62Q.png" },
+    { id: "supercopa_tr", nome: "Turkish Super Cup", tipo: "supercopa", div: 1, logo: "https://static.flashscore.com/res/image/data/E7B8z2Sp-I3pO0ATD.png" },
+
+    // BELGICA
+    { id: "be_1", nome: "Jupiler Pro League", tipo: "liga", div: 1, logo: "https://r2.thesportsdb.com/images/media/league/badge/mjit7n1593634474.png/medium" },
+
+    // ESCOCIA
+    { id: "sco_1", nome: "Scottish Premiership", tipo: "liga", div: 1, logo: "https://tmssl.akamaized.net//images/logo/header/sc1.png?lm=1750156806" },
+
+    // ARÁBIA
+    { id: "ara_1", nome: "Saudi Pro League", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/saudi-arabia/512x512/saudi-professional-league.59d5c8af.png" },
+    { id: "copa_ara", nome: "Kings Cup", tipo: "copa", div: 1, logo: "https://r2.thesportsdb.com/images/media/league/badge/bej7om1754244351.png/medium" },
+    { id: "supercopa_ara", nome: "Saudi Super Cup", tipo: "supercopa", div: 1, logo: "https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Saudi_Super_Cup_Logo.jpg/250px-Saudi_Super_Cup_Logo.jpg" },
+    
+    //Nigeria
+    { id: "nga_1", nome: "Nigeria Professional Football League", tipo: "liga", div: 1, logo: "https://upload.wikimedia.org/wikipedia/pt/thumb/d/d9/NPFL_Nigeria_logo.png/250px-NPFL_Nigeria_logo.png" },
+
+    // EUA
+    { id: "usa_1", nome: "MLS", tipo: "liga", div: 1, logo: "https://assets.football-logos.cc/logos/usa/512x512/mls.389fae67.png" },
+    { id: "copa_usa", nome: "Open Cup", tipo: "copa", div: 1, logo: "https://imgs.search.brave.com/pB6PgZu_OW3OHb4_ZBjUxzDwUhSjhle8O9UPuszeuyo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvcHQvNC80NS9M/YW1hcl9IdW50X1Uu/Uy5fT3Blbl9DdXAu/cG5n" },
+    { id: "supercopa_usa", nome: "Leagues Cup", tipo: "supercopa", div: 1, logo: "https://imgs.search.brave.com/DHfFYR3peU25mv1w5pjDIOYRaLG--qbJLhJxclTRQsI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMTcvTGVh/Z3Vlcy1DdXAtSWRl/bnRpdHktVmlzdWFs/LUNvbmNlcHQtUE5H/LnBuZw" },
+    
+    // MEXICO
+    { id: "mx_1", nome: "Liga MX Apertura", tipo: "liga", div: 1, logo: "https://tmssl.akamaized.net//images/logo/homepageWappen150x150/mexa.png?lm=1406287644" },
+    { id: "copa_mx", nome: "Copa Mexico", tipo: "copa", div: 1, logo: "https://static.flashscore.com/res/image/data/rPEGnQiU-UcAdKF4S.png" },
+    { id: "supercopa_mx", nome: "Campeón de Campeones", tipo: "supercopa", div: 1, logo: "https://static.flashscore.com/res/image/data/MqXU6r7j-AZnE56jI.png" },
+
+    //Costa Do Marfim
+    { id: "civ_1", nome: "Ligue 1 Côte d'Ivoire", tipo: "liga", div: 1, logo: "https://static.flashscore.com/res/image/data/4YaESgjD-2J0GSfL3.png" },
+
+    
+    
+
+    // CONTINENTAIS
+    { id: "uefa_cl", nome: "Champions League", tipo: "continental", div: 1, logo: "https://imgs.search.brave.com/CsMnDkvWxehW_hbGRdsq7z8KeL6NchGzeJoNRU9dBt0/rs:fit:100:100:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5hcGktc3BvcnRz/LmlvL2Zvb3RiYWxs/L2xlYWd1ZXMvMi5w/bmc" },
+    { id: "uefa_el", nome: "Europa League", tipo: "continental", div: 2, logo: "https://cdn-img.staticzz.com/img/logos/competicoes/28_imgbank_el_20240625151457.png" },
+    { id: "uefa_col", nome: "Conference League", tipo: "continental", div: 3, logo: "https://cdn-img.staticzz.com/img/logos/competicoes/5932_imgbank_ecl_20250220174704.png" },
+    { id: "conmebol_lib", nome: "Libertadores", tipo: "continental", div: 1, logo: "https://upload.wikimedia.org/wikipedia/pt/thumb/9/95/Conmebol_Libertadores_logo.svg/330px-Conmebol_Libertadores_logo.svg.png" },
+    { id: "conmebol_sul", nome: "Sulamericana", tipo: "continental", div: 2, logo: "https://logodownload.org/wp-content/uploads/2018/10/copa-sulamericana-logo.png" },
+    { id: "concacaf_clc", nome: "Concacaf Champions Cup", tipo: "continental", div: 1, logo: "https://assets.football-logos.cc/logos/tournaments/512x512/concacaf-champions-cup.fcd3ac56.png" },
+    { id: "afc_cla", nome: "AFC Champions", tipo: "continental", div: 1, logo: "https://assets.football-logos.cc/logos/tournaments/512x512/afc-champions-league-elite.bb0766a4.png" },
+
+    // ==========================================
+    // 🌍 SUPERCOPAS E INTERCONTINENTAL (NOVOS)
+    // ==========================================
+    { 
+        id: "uefa_supercup", 
+        nome: "Supercopa da UEFA", 
+        tipo: "supercopa_continental", 
+        div: 1, 
+        logo: "https://imgs.search.brave.com/SUjf_50aGPX-WN0kp0p9pLrYkh0MdMFQC75Qi88U614/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzMwLzIvdWVmYS1z/dXBlci1jdXAtbG9n/by1wbmdfc2Vla2xv/Z28tMzA5OTcyLnBu/Zw" 
+    },
+    { 
+        id: "conmebol_recopa", 
+        nome: "Recopa Sudamericana", 
+        tipo: "supercopa_continental", 
+        div: 1, 
+        logo: "https://upload.wikimedia.org/wikipedia/pt/thumb/2/2f/Conmebol_Recopa_logo_%28after_2017%29.png/250px-Conmebol_Recopa_logo_%28after_2017%29.png" 
+    },
+    { 
+        id: "intercontinental_cup", 
+        nome: "Copa Intercontinental da FIFA", 
+        tipo: "torneio_intercontinental", 
+        div: 1, 
+        logo: "https://imgs.search.brave.com/V7dNQSyb4ifU7YW0QBLB-YFnnJpJGSj6uVP73Yy8UCE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aW1nLnN0YXRpY3p6/LmNvbS9pbWcvbG9n/b3MvY29tcGV0aWNv/ZXMvNzM5Ml9pbWdi/YW5rX2ZpY18yMDI0/MTIxMTE3NTA1Ni5w/bmc" 
+    },
+
+    // ==========================================
+    // 🌍 SELEÇÕES E COMPETIÇÕES INTERNACIONAIS
+    // ==========================================
+    { id: "sel_amistoso", nome: "Amistosos Internacionais", tipo: "selecao", div: 0, logo: "https://flagcdn.com/w160/un.png" },
+    { id: "sel_elim_wc", nome: "Eliminatórias da Copa do Mundo", tipo: "selecao", div: 0, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/FIFA_World_Cup_2026_Logo.svg/250px-FIFA_World_Cup_2026_Logo.svg.png" },
+    { id: "sel_copa_mundo", nome: "Copa do Mundo", tipo: "selecao", div: 1, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/FIFA_World_Cup_2026_Logo.svg/250px-FIFA_World_Cup_2026_Logo.svg.png" },
+    { id: "sel_olimpiadas", nome: "Olimpíadas", tipo: "selecao", div: 0, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Olympic_rings_without_rims.svg/250px-Olympic_rings_without_rims.svg.png" },
+    { id: "sel_euro", nome: "Eurocopa", tipo: "selecao", div: 1, logo: "https://imgs.search.brave.com/wYZ-hTdsFy_ivTeIgumjBKicssr0dMM8s0RhZ7SlQEs/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/Mi8yNi9VRUZBX0V1/cm9fMjAyNF9Mb2dv/LnN2Zy8yNTBweC1V/RUZBX0V1cm9fMjAy/NF9Mb2dvLnN2Zy5w/bmc" },
+    { id: "sel_copa_america", nome: "Copa América", tipo: "selecao", div: 1, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Copa_Am%C3%A9rica_logo.svg/250px-Copa_Am%C3%A9rica_logo.svg.png" },
+    { id: "sel_euro_qualy", nome: "Eliminatórias da Eurocopa", tipo: "selecao", div: 0, logo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/UEFA_Euro_2024_Logo.svg/250px-UEFA_Euro_2024_Logo.svg.png" },
+    { id: "sel_nations_a", nome: "Nations League A", tipo: "selecao", div: 1, logo: "https://imgs.search.brave.com/QM-F1qzO3iVbd_ti0jF0x2SUwcnXTcKilDfMzHwrSvQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/OC84MC9VRUZBX05h/dGlvbnNfTGVhZ3Vl/LnN2Zy8yNTBweC1V/RUZBX05hdGlvbnNf/TGVhZ3VlLnN2Zy5w/bmc" },
+    { id: "sel_nations_b", nome: "Nations League B", tipo: "selecao", div: 2, logo: "https://imgs.search.brave.com/QM-F1qzO3iVbd_ti0jF0x2SUwcnXTcKilDfMzHwrSvQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/OC84MC9VRUZBX05h/dGlvbnNfTGVhZ3Vl/LnN2Zy8yNTBweC1V/RUZBX05hdGlvbnNf/TGVhZ3VlLnN2Zy5w/bmc" },
+    { id: "sel_nations_c", nome: "Nations League C", tipo: "selecao", div: 3, logo: "https://imgs.search.brave.com/QM-F1qzO3iVbd_ti0jF0x2SUwcnXTcKilDfMzHwrSvQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/OC84MC9VRUZBX05h/dGlvbnNfTGVhZ3Vl/LnN2Zy8yNTBweC1V/RUZBX05hdGlvbnNf/TGVhZ3VlLnN2Zy5w/bmc" },
+    { id: "sel_nations_d", nome: "Nations League D", tipo: "selecao", div: 4, logo: "https://imgs.search.brave.com/QM-F1qzO3iVbd_ti0jF0x2SUwcnXTcKilDfMzHwrSvQ/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvZW4vdGh1bWIv/OC84MC9VRUZBX05h/dGlvbnNfTGVhZ3Vl/LnN2Zy8yNTBweC1V/RUZBX05hdGlvbnNf/TGVhZ3VlLnN2Zy5w/bmc" }
+];
